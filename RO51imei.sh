d3663b58e98eb579d2993b07ab78c584bb8d5067
terence <99847498+julius507@users.noreply.github.com>
2022-08-07 15:57:40 +0800

    Update RO51imei.sh

--- RO51imei.sh
+++ RO51imei.sh
@@ -2,6 +2,6 @@
 echo "Changing IMEI commenced"
 ubus call version set_atcmd_info '{"atcmd":"AT+FLASHBP=0"}'
 ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=D"}'
-ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=W,0101,01NOV2012,355313080034935"}'
+ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=W,0101,01NOV2012,355313086615562"}'
 
 
